cbac32ba2e9d930e1739a4f00d508b79daa62f14
Abhithakur04 <152187846+Abhithakur04@users.noreply.github.com>
2025-05-16 23:46:15 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';  
+const PORT = process.env.PORT || 5000;
 const connectDB=require("./configure/db");
 
 dotenv.config();
@@ -20,6 +21,8 @@ app.use(cors({
   origin: FRONTEND_URL,
   credentials: true,
 }));
+
+
 
 // Security headers
 app.use(helmet());
@@ -45,7 +48,7 @@ app.use('/api', aadhaarRoutes);
  connectDB()
   .then(()=>{
     console.log("Succesfuly connect to database");
-     app.listen(process.env.PORT,()=>{
+     app.listen(PORT,()=>{
         console.log("Successfully server start");
     });
   })
