6a6387c7e64900fde560f465ef1375a7443c42fa
Abhithakur04 <152187846+Abhithakur04@users.noreply.github.com>
2025-05-16 23:37:04 +0530

    Update api.js

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -10,3 +10,4 @@ const API = axios.create({
 export const loginUser = (data) => API.post('/auth/login', data);
 export const registerUser = (data) => API.post('/auth/register', data);
 export const getAadhaarStatus = (data) => API.post('/aadhaar-status', data);
+export const logoutUser = () => API.post('/auth/logout'); 
