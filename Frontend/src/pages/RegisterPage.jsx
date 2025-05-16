0e44874d7125246fec962565e50741d947b6e97b
Abhithakur04 <152187846+Abhithakur04@users.noreply.github.com>
2025-05-17 02:30:31 +0530

    Update RegisterPage.jsx

--- Frontend/src/pages/RegisterPage.jsx
+++ Frontend/src/pages/RegisterPage.jsx
@@ -1,11 +1,12 @@
 import RegisterForm from '../components/RegisterForm';
 
-const RegisterPage = ({ setToken, setRole }) => {
+const RegisterPage = () => {
   return (
     <div className="min-h-screen bg-gray-50 flex items-center justify-center">
-      <RegisterForm setToken={setToken} setRole={setRole} />
+      <RegisterForm />
     </div>
   );
 };
 
 export default RegisterPage;
+
